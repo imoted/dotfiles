b6fda787009e993e09da863dc62d360d718bf4d8
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2018-07-20 20:54:04 +0900

    [fix](dotfilesLink) file pathが間違っていたので修正
    __Summary__ ファイルパス修正
    * Change files
     dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,8 +5,8 @@ sudo apt-get -y install clang-format-3.6
 sudo apt-get -y install python-jedi
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd) #cd current directory
-ln -sf ./.vimrc ~/.vimrc
-ln -sf ./.screenrc ~/.screenrc
-ln -sf ./ftdetect ~/.vim
-ln -sf ./bundle ~/.vim
-ln -sf ./snipets ~/.vim
+ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
+ln -sf $SCRIPT_DIR/.screenrc ~/.screenrc
+ln -sf $SCRIPT_DIR/ftdetect ~/.vim
+ln -sf $SCRIPT_DIR/bundle ~/.vim
+ln -sf $SCRIPT_DIR/snipets ~/.vim
