5c2441d56cc3f6e13c21f92f5b8c5d701a12775a
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2018-10-25 11:48:56 +0900

    add autopep8

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,8 @@ sudo apt-get -y install vim-nox-py2
 sudo apt-get -y install flake8
 sudo apt-get -y install clang-format-3.6
 sudo apt-get -y install python-jedi
+sudo apt-get -y install python-pip
+pip install autopep8
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd) #cd current directory
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
